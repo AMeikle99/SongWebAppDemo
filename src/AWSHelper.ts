c7d42487e4f51898a5da765b8fc5e549006d8982
AMeikle99 <aidenmeikle@ymail.com>
2020-11-25 13:49:41 +0000

    Add catch to song request apis

--- src/AWSHelper.ts
+++ src/AWSHelper.ts
@@ -6,22 +6,22 @@ export default class AWSHelper {
 	private SONG_API_URL = process.env.REACT_APP_API_URL || ''
 	
 	getAllSongs = async () => {
-		const response = await axios.get(this.SONG_API_URL)
-		console.log(response)
-		console.log(this.SONG_API_URL)
-		const songList = this.modelSongResponse(response)
-		return songList
+		return await axios.get(this.SONG_API_URL).then(response => {
+			console.log(response)
+			const songList = this.modelSongResponse(response)
+			return songList
+		}).catch(() => {
+			return []
+		})
+		
 	}
 
 	submitNewSong = (song: Song) => {
-		axios.post(this.SONG_API_URL, song).then(response => {
-			console.log(response)
-		})
-		console.log(song)
+		axios.post(this.SONG_API_URL, song)
 	}
 
 	private modelSongResponse = (response: AxiosResponse) => {
-		const responseData = response.data
+		const responseData = response.data === undefined ? [] : response.data
 		
 		return responseData.map((song: Song) => {
 			return {
--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <h1>AWS Simple Web App Demo</h1>
         <h5 className='mt-5'>Below is a Table of Songs loaded from a DynamoDB Table from AWS</h5>
         <h5>The data is requested via API Gateway</h5>
-        <h5>This Web Server is hosted using S3</h5>
+        <h5>This Web Server is hosted using {process.env.REACT_APP_HOSTING_SERVICE || '<Unknown>'}</h5>
       </div>
       <div className='col-6 mx-auto'>
         <SongTable/>
